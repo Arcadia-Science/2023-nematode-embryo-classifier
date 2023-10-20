75bc162aed9c5dbe441be3a766da35807a45a9ec
Keith Cheveralls <keith.chev@gmail.com>
2023-10-20 13:46:01 -0700

    add back CLI for nd2omezarr and remove absolute paths
    add back CLI for nd2omezarr and remove absolute paths

--- src/embryostage/scripts/nd2omezarr.py
+++ src/embryostage/scripts/nd2omezarr.py
@@ -1,30 +1,16 @@
 # %% imports
 from pathlib import Path
+import click
 import nd2
 import numpy as np
+
 from iohub import open_ome_zarr
 from tqdm import tqdm
 
 
-nd2_path = (
-    r"/mnt/embryostage-local/celegans_embryos_dataset/"
-    "raw_data/100423/ChannelDIC_40x_Seq0012.nd2"
-)
-# We use the strain_condition_date_raw.zarr naming convention
-ome_zarr_path = (
-    r"/mnt/embryostage-local/celegans_embryos_dataset/"
-    "raw_data/231004_TBD_control_raw.zarr"
-)
-
-channel_names = ["DIC40x"]
-
-
-# %%
-
-
-# @click.option("--nd2path", type=Path, help="Path to nd2 file")
-# @click.option("--ome_zarr_path", type=Path, help="Path to output zarr")
-# @click.command()
+@click.option("--nd2_path", type=Path, help="Path to nd2 file")
+@click.option("--ome_zarr_path", type=Path, help="Path to output zarr")
+@click.command()
 def nd2omezarr(nd2_path: str, ome_zarr_path: str) -> None:
     """
     Converts a nd2 file to an ome-zarr file.
@@ -36,15 +22,17 @@ def nd2omezarr(nd2_path: str, ome_zarr_path: str) -> None:
     Returns:
         None
     """
+    channel_names = ["DIC40x"]
+
     # Read the nd2 file as a dask array
     nd2dask = nd2.imread(nd2_path, dask=True)
 
     # Get the dimensions of the nd2 file
     T, P, Y, X = nd2dask.shape
-    
+
     # Loop through each position in the nd2 file
     print(f"converting {nd2_path} to {ome_zarr_path}")
-    
+
     for pos in tqdm(range(P)):
         # Get the dask array for the current position
         pos_dask = nd2dask[:, pos, :, :].rechunk(chunks=(T, Y, X))
@@ -54,8 +42,9 @@ def nd2omezarr(nd2_path: str, ome_zarr_path: str) -> None:
         pos_zarr_path.mkdir(parents=True, exist_ok=True)
 
         # Open the ome-zarr file for the current position
-        pos_zarr = open_ome_zarr(pos_zarr_path, layout="fov", mode="w",
-                                 channel_names=channel_names)
+        pos_zarr = open_ome_zarr(
+            pos_zarr_path, layout="fov", mode="w", channel_names=channel_names
+        )
 
         # Convert the dask array to a numpy array and reshape it
         pos_array = np.asarray(pos_dask).reshape(T, 1, 1, Y, X)
@@ -68,4 +57,4 @@ def nd2omezarr(nd2_path: str, ome_zarr_path: str) -> None:
 
 
 if __name__ == "__main__":
-    nd2omezarr(nd2_path, ome_zarr_path)
+    nd2omezarr()
