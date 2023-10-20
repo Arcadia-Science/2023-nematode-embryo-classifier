bcf7bc35fa34002f1deca8d33ac416811865ae42
Keith Cheveralls <keith.chev@gmail.com>
2023-10-20 13:45:28 -0700

    in embryo_finder module, delete commented-out code, make plot_results a private instance method, and rename the module to conform to lower snake case

--- src/embryostage/preprocess/embryo_finder.py
+++ src/embryostage/preprocess/embryo_finder.py
@@ -103,6 +103,8 @@ class EmbryoFinder:
         self.FOVs = FOVs
         # bioformats2raw outputs data at multiple resolutions.
         # We use the highest resolution for analysis.
+        # TODO (KC): bioformats2raw is no longer used to convert nd2 to zarr,
+        # so this may not be necessary
         self.pyramid_level = "0"
         self.output_path = output_path
         self.strain = strain
@@ -119,76 +121,19 @@ class EmbryoFinder:
         Find and crop C. elegans embryos in the time series.
         """
 
-        def plot_results(method):  # TODO: turn into a napari layer or widget.
-            """plot_results private subfunction to visualize the analysis results."""
-
-            if method == "otsu":
-                # Clear existing plots.
-                plt.figure(1).clear()
-                plt.figure(1).set_size_inches(20, 5)
-                plt.draw()
-                # Plot the segmentation results.
-
-                plt.subplot(1, 3, 1)
-                plt.imshow(time_series_std, cmap="gray")
-                plt.title("std along time")
-                plt.axis("off")
-
-                plt.subplot(1, 3, 2)
-                plt.imshow(smooth_projection, cmap="gray")
-                plt.title("smoothed in xy")
-                plt.axis("off")
-
-                plt.subplot(1, 3, 3)
-                plt.imshow(mask, cmap="gray")
-                plt.title("segmented embryo candidates")
-
-                plt.axis("off")
-
-                for n in range(len(embryos_fov)):
-                    plt.text(
-                        embryos_fov[n]["xmin"],
-                        embryos_fov[n]["ymin"],
-                        str(n),
-                        color="green",
-                        fontsize=12,
-                    )
-
-                plt.title(f"{len(embryos_fov)} embryos detected")
-                plt.draw()
-                plt.pause(0.001)
-
-                # Make a folder if it doesn't exist and save the plot.
-                fovPath = Path(
-                    self.output_path,
-                    self.strain,
-                    self.perturbation,
-                    f"{self.date_stamp}_{fov}",
-                )
-                fovPath.mkdir(parents=True, exist_ok=True)
-                plt.savefig(Path(fovPath, "detected_embryos.png"))
-
-            elif method == "hough":
-                pass
-            else:
-                return
-
         # Load the time series data and compute the time projection.
         for i, fov in enumerate(self.FOVs):
             zarr_path = Path(self.input_path, f"{fov}")
             if not zarr_path.exists():
                 print(f"Path {zarr_path} does not exist. Skipping FOV {fov}.")
                 continue
+
             # Load the time series data and compute the time projection.
             print(f"Processing FOV {fov}.")
             with open_ome_zarr(
-                Path(self.input_path, f"{fov}"),
-                layout="fov",
-                mode="r",
-                channel_names="BF20x",
+                Path(self.input_path, f"{fov}"), layout="fov", mode="r", channel_names="BF20x"
             ) as input_store:
                 time_series = np.asarray(input_store[self.pyramid_level])
-                _ = time_series.shape[0]
                 time_series_std = np.std(time_series, axis=0).squeeze()
 
                 # Smoothing avoids detection of very small objects and
@@ -198,13 +143,18 @@ class EmbryoFinder:
                     sigma=self.d_embryo_pix // 10,
                 )
 
-                (
-                    embryos_fov,
-                    mask,
-                    regions,
-                ) = self.segment_time_projection(smooth_projection, method="otsu")
+                (embryos_fov, mask, regions) = self.segment_time_projection(
+                    smooth_projection, method="otsu"
+                )
 
-                plot_results("otsu")
+                self._plot_results(
+                    method="otsu",
+                    fov=fov,
+                    embryos_fov=embryos_fov,
+                    time_series_std=time_series_std,
+                    smooth_projection=smooth_projection,
+                    mask=mask,
+                )
 
             # Save the cropped embryos to the output zarr store
             # organized by strain, perturbation, fov, and embryo index.
@@ -238,72 +188,61 @@ class EmbryoFinder:
                     embryo["ymin"] : embryo["ymax"],
                     embryo["xmin"] : embryo["xmax"],
                 ]
-                # ypad1 = (output_store.shape[3] - cropped_series.shape[3]) // 2
-                # ypad2 = output_store.shape[3] - cropped_series.shape[3] - ypad1
-                # xpad1 = (output_store.shape[4] - cropped_series.shape[4]) // 2
-                # xpad2 = output_store.shape[4] - cropped_series.shape[4] - xpad1
-
-                # cropped_series = np.pad(
-                #     cropped_series,
-                #     pad_width=(
-                #         (0, 0),
-                #         (0, 0),
-                #         (0, 0),
-                #         (ypad1, ypad2),
-                #         (xpad1, xpad2),
-                #     ),
-                #     mode="symmetric",
-                # )
 
                 output_store[:] = cropped_series
 
-                # Attempted rechunking while maintaining metadata,
-                # but that didn't work.
-                # # Rechunk the data to a more convenient format
-                # for downstream transfer.
-                # zarr_path = Path(embryo_path, "0")
-                # zarr_store = zarr.open(zarr_path, mode="r+")
-                # rechunk(
-                #     zarr_store,
-                #     target_chunks=(
-                #         t_points,
-                #         1,
-                #         1,
-                #         x_shape,
-                #         y_shape,
-                #     ),
-                # )
-                #         t_points,
-                #         1,
-                #         1,
-                #         ,
-                #         ,
-                #     ),
-                #     max_mem="256MB",
-                #     target_store=zarr_path,
-                #     temp_store=f"{self.output_path}/rechunking_temp",
-                # )
+    def _plot_results(
+        self, method, fov, embryos_fov, time_series_std, smooth_projection, mask
+    ):
+        """visualize the analysis results generated in find_embryos"""
+        # TODO: turn this into a napari layer or widget.
 
-                # Current napari-ome-zarr plugin requires wells organized in a
-                # rectangular grid and cannot load this data.
-                # If it becomes possible to load this data, the following code
-                # can be used to save the embryos in an HCS layout.
-                # with open_ome_zarr(
-                #     f"{self.output_path}",
-                #     layout="hcs",
-                #     mode="a",
-                #     channel_names=["BF"],
-                # ) as output_store:
-                #     position = output_store.create_position(
-                #         self.perturbation, f"fov{fov}", f"{i}"
-                #     )
-                #     position[self.pyramid_level] = time_series[
-                #         :,
-                #         :,
-                #         :,
-                #         embryo["bbox"][0] : embryo["bbox"][2],
-                #         embryo["bbox"][1] : embryo["bbox"][3],
-                #     ]
+        if method == "otsu":
+            plt.figure(1).clear()
+            plt.figure(1).set_size_inches(20, 5)
+            plt.draw()
+
+            plt.subplot(1, 3, 1)
+            plt.imshow(time_series_std, cmap="gray")
+            plt.title("std along time")
+            plt.axis("off")
+
+            plt.subplot(1, 3, 2)
+            plt.imshow(smooth_projection, cmap="gray")
+            plt.title("smoothed in xy")
+            plt.axis("off")
+
+            plt.subplot(1, 3, 3)
+            plt.imshow(mask, cmap="gray")
+            plt.title("segmented embryo candidates")
+
+            plt.axis("off")
+
+            for n in range(len(embryos_fov)):
+                plt.text(
+                    embryos_fov[n]["xmin"],
+                    embryos_fov[n]["ymin"],
+                    str(n),
+                    color="green",
+                    fontsize=12,
+                )
+
+            plt.title(f"{len(embryos_fov)} embryos detected")
+            plt.draw()
+            plt.pause(0.001)
+
+            # Make a folder if it doesn't exist and save the plot.
+            fovPath = Path(
+                self.output_path,
+                self.strain,
+                self.perturbation,
+                f"{self.date_stamp}_{fov}",
+            )
+            fovPath.mkdir(parents=True, exist_ok=True)
+            plt.savefig(Path(fovPath, "detected_embryos.png"))
+
+        elif method == "hough":
+            raise NotImplementedError
 
     def segment_time_projection(self, time_projection, method="otsu"):
         """
@@ -326,10 +265,8 @@ class EmbryoFinder:
             List of region properties for the segmented embryos.
         """
         if method == "otsu":
-            # Otsu thresholding
             binary = time_projection > threshold_otsu(time_projection)
         elif method == "hough":
-            # Hough transform
             pass
 
         # Remove small objects and clear the border.
@@ -341,9 +278,10 @@ class EmbryoFinder:
         # dictionary of bounding box coordinates and cropped image.
         labeled = label(binary)
         regions = regionprops(labeled)
-        embryos = []
 
+        embryos = []
         for n in range(len(regions)):
+            # TODO (KC): document the origin of these magic numbers
             looks_like_embryo = (
                 (0.5 * self.l_embryo_pix <= regions[n].major_axis_length <= self.l_embryo_pix)
                 and (
@@ -357,10 +295,12 @@ class EmbryoFinder:
             # Ensure that uniform square crops are taken around the embryo.
             center_y = (regions[n].bbox[0] + regions[n].bbox[2]) // 2
             center_x = (regions[n].bbox[1] + regions[n].bbox[3]) // 2
+
             xmin = int(center_x - self.l_embryo_pix // 2)
             xmax = int(center_x + self.l_embryo_pix // 2)
             ymin = int(center_y - self.l_embryo_pix // 2)
             ymax = int(center_y + self.l_embryo_pix // 2)
+
             embryo_within_image = (
                 xmin >= 0
                 and ymin >= 0
