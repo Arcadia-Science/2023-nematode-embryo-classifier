33dd07da095d278a6ac52c6ead6836d726bb96bc
Keith Cheveralls <keith.chev@gmail.com>
2023-10-24 12:10:45 -0700

    add fov_ids to the CLI args in view_embryos

--- src/embryostage/scripts/view_embryos.py
+++ src/embryostage/scripts/view_embryos.py
@@ -7,18 +7,18 @@ from embryostage.preprocess.utils import get_movie_paths
 
 
 @click.option("--data-dirpath", type=pathlib.Path, help="Path to the data directory")
-@click.option("--dataset-id", type=str, help="The ID of the dataset to process")
+@click.option("--dataset-id", type=str, help="The ID of the dataset")
+@click.option("--fov-ids", type=str, help="The IDs of the FOVs as a comma-separated list")
 @click.command()
-def view_embryos(data_dirpath, dataset_id):
+def view_embryos(data_dirpath, dataset_id, fov_ids):
     '''
     Load the timelapses for all embryos from one or more FOVs from a given dataset
     into napari for viewing and annotating the embryos.
     '''
-
     viewer = napari.Viewer()
 
-    fov_ids = [0, 1, 2, 3]
-    movie_paths = get_movie_paths(data_dirpath, dataset_id, fov_ids=list(map(str, fov_ids)))
+    fov_ids = [fov_id.strip() for fov_id in fov_ids.split(",")]
+    movie_paths = get_movie_paths(data_dirpath, dataset_id, fov_ids=fov_ids)
 
     for movie_path in movie_paths:
         movie = zarr.open(str(movie_path), mode="r")
