f784de1a0b8d359933420db8577ac1ca866240c3
Keith Cheveralls <keith.chev@gmail.com>
2023-10-26 15:39:15 -0700

    rewrite train_models script as a CLI

--- src/embryostage/metadata.py
+++ src/embryostage/metadata.py
@@ -3,18 +3,36 @@ import pandas as pd
 from embryostage import file_utils
 
 
+def get_dataset_metadata_filepath():
+    '''
+    the manually-maintained CSV file of dataset metadata
+    '''
+    filepath = file_utils.find_repo_root(__file__) / "ground_truth" / "embryo_metadata.csv"
+    if not filepath.exists():
+        raise FileNotFoundError(f"Dataset metadata file not found at {filepath}")
+    return filepath
+
+
+def get_annotations_filepath():
+    '''
+    the CSV of manual annotations of embryo developmental stages
+    '''
+    filepath = (
+        file_utils.find_repo_root(__file__) / "ground_truth" / "embryo_developmental_stage.csv"
+    )
+    if not filepath.exists():
+        raise FileNotFoundError(f"Manual annotations file not found at {filepath}")
+    return filepath
+
+
 def load_dataset_metadata(dataset_id):
     '''
     Load the metadata for a given dataset as a pandas series
     '''
-    repo_dirpath = file_utils.find_repo_root(__file__)
-    dataset_metadata_filepath = repo_dirpath / "ground_truth" / "embryo_metadata.csv"
-    if not dataset_metadata_filepath.exists():
-        raise FileNotFoundError(f"Metadata file not found at {dataset_metadata_filepath}")
-
+    dataset_metadata_filepath = get_dataset_metadata_filepath()
     dataset_metadata = pd.read_csv(dataset_metadata_filepath)
 
-    # the dataset_id is a date of the from `YYMMDD` which is read as an integer
+    # the dataset_id is a date of the form `YYMMDD` which is read as an integer
     dataset_metadata['dataset_id'] = dataset_metadata['dataset_id'].astype(str)
 
     dataset_metadata = dataset_metadata.loc[dataset_metadata.dataset_id == dataset_id]
--- src/embryostage/scripts/train_models.py
+++ src/embryostage/scripts/train_models.py
@@ -1,101 +1,72 @@
-# %% imports
-from pathlib import Path
+import click
 import pytorch_lightning as pl
 
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_model_summary import summary
 
+from embryostage.cli import options as cli_options
+from embryostage.metadata import get_annotations_filepath, get_dataset_metadata_filepath
 from embryostage.models.classification import SulstonNet
 from embryostage.models.data import EmbryoDataModule
 
 RANDOM_SEED = 2023
 pl.seed_everything(RANDOM_SEED)
 
-# %load_ext autoreload
-# %autoreload 2
-# %% Construct the data module
 
+@cli_options.data_dirpath_option
+@click.command()
+def train_models(data_dirpath):
+    '''
+    train models for embryo classification using different combinations of channels
+    '''
+    channel_combinations = {
+        "sulstonNet_heatshock_7classes_raw": ["raw"],
+        "sulstonNet_heatshock_7classes_moving_mean_std": [
+            "moving_mean",
+            "moving_std",
+        ],
+    }
 
-dataset_path = Path("~/docs/data/predict_development/celegans_embryos_dataset").expanduser()
-annotation_csv = Path(
-    "~/docs/code/2023-celegans-sandbox/ground_truth/embryo_developmental_stage.csv"
-).expanduser()
+    for experiment_name, channel_names in channel_combinations.items():
+        print(f"Training model: {experiment_name}\n")
 
-metadata_csv = Path(
-    "~/docs/code/2023-celegans-sandbox/ground_truth/embryo_metadata.csv"
-).expanduser()
-
-channel_combinations = {
-    "sulstonNet_heatshock_7classes_raw": ["raw"],
-    "sulstonNet_heatshock_7classes_moving_mean_std": [
-        "moving_mean",
-        "moving_std",
-    ],
-}
-
-for experiment_name, channel_names in channel_combinations.items():
-    print(f"Training model: {experiment_name}\n_________________________")
-
-    embryo_data_module = EmbryoDataModule(
-        dataset_path,
-        channel_names,
-        annotation_csv,
-        metadata_csv,
-        batch_size=100,
-        balance_classes=True,
-    )
-    embryo_data_module.setup()
-
-    # %% Instantiate and examine the model
-    model = SulstonNet(
-        in_channels=len(channel_names),
-        n_classes=embryo_data_module.dataset.n_classes,
-        index_to_label=embryo_data_module.dataset.index_to_label,
-    )
-
-    # model_graph = draw_graph(
-    #     model,
-    #     model.example_input_array,
-    #     directory=Path(dataset_path, "models"),
-    #     graph_name=experiment_name,
-    #     save_graph=True,
-    # )
-
-    print(
-        summary(
-            model,
-            model.example_input_array,
-            show_hierarchical=False,
-            max_depth=2,
+        embryo_data_module = EmbryoDataModule(
+            data_dirpath,
+            channel_names,
+            annotations_csv=get_annotations_filepath(),
+            metadata_csv=get_dataset_metadata_filepath(),
+            batch_size=100,
+            balance_classes=True,
         )
-    )
+        embryo_data_module.setup()
 
-    # %% Train the model
-    logger = pl.loggers.TensorBoardLogger(
-        Path(dataset_path, "models"),
-        version=f"{experiment_name}",
-        log_graph=True,
-    )
+        model = SulstonNet(
+            in_channels=len(channel_names),
+            n_classes=embryo_data_module.dataset.n_classes,
+            index_to_label=embryo_data_module.dataset.index_to_label,
+        )
+        print(summary(model, model.example_input_array, show_hierarchical=False, max_depth=2))
 
-    # saves top-K checkpoints based on "val_loss" metric
-    checkpoint_callback = ModelCheckpoint(
-        save_top_k=5,
-        monitor="val_loss",
-        mode="min",
-        filename="checkpoint-{epoch:02d}-{val_loss:.2f}",
-    )
+        logger = pl.loggers.TensorBoardLogger(
+            data_dirpath / "models", version=f"{experiment_name}", log_graph=True
+        )
 
-    trainer = pl.Trainer(
-        max_epochs=20,
-        logger=logger,
-        log_every_n_steps=10,
-        callbacks=[checkpoint_callback],
-    )
+        # saves top-K checkpoints based on "val_loss" metric
+        checkpoint_callback = ModelCheckpoint(
+            save_top_k=5,
+            monitor="val_loss",
+            mode="min",
+            filename="checkpoint-{epoch:02d}-{val_loss:.2f}",
+        )
 
-    trainer.fit(
-        model,
-        embryo_data_module,
-    )
+        trainer = pl.Trainer(
+            max_epochs=20, logger=logger, log_every_n_steps=10, callbacks=[checkpoint_callback]
+        )
 
-    test_outputs = trainer.test(model, embryo_data_module.test_dataloader())
-    print(test_outputs)
+        trainer.fit(model, embryo_data_module)
+        test_outputs = trainer.test(model, embryo_data_module.test_dataloader())
+        print(test_outputs)
+
+
+if __name__ == "__main__":
+    train_models()
